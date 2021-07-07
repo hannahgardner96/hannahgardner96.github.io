ec3e3b1a61cf4f07df8fef0695c2d19bd60ac0f2
Hannah Gardner <hannahgardner@Hannahs-MacBook-Pro.local>
2021-07-06 22:37:04 -0500

    portfolio website largely complete

---
+++ porfolio.js
@@ -0,0 +1,19 @@
+const navProjects = document.getElementById("projects-nav")
+const navSkills = document.getElementById("skills-nav")
+const navResume = document.getElementById("resume-nav")
+const navContact = document.getElementById("contact-nav")
+const seeMyWorkBtn = document.getElementById("see-my-work-btn")
+const projectsStart = document.getElementById("projects-start")
+const skillsStart = document.getElementById("skills-start")
+const resumeStart = document.getElementById("resume-start")
+const contactStart = document.getElementById("contact-start")
+
+navProjects.addEventListener("click", () => projectsStart.scrollIntoView({behavior: 'smooth'}))
+
+seeMyWorkBtn.addEventListener("click", () => projectsStart.scrollIntoView({behavior: 'smooth'}))
+
+navSkills.addEventListener("click", () => skillsStart.scrollIntoView({behavior: 'smooth'}))
+
+navResume.addEventListener("click", () => resumeStart.scrollIntoView({behavior: 'smooth'}))
+
+navContact.addEventListener("click", () => contactStart.scrollIntoView({behavior: 'smooth'}))
